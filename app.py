b8de4d2c19c493e232305fbe8dc70f1c5a1f8766
Veeraj Panchal <panchalveeraj12@gmail.com>
2026-06-12 18:46:04 +0530

    fix : satisfy linter spacing rules at end of file

--- app.py
+++ app.py
@@ -75,4 +75,3 @@ if __name__ == '__main__':
     # Render dynamic port pass karega, local par automatic 5000 use hoga
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    
